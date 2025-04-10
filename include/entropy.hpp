954b40171148e650fb86a6c2325b5cbab0fc774b
schauss <schauss@dmanz.org>
2025-04-10 12:20:50 -0700

    noice

---
+++ include/entropy.hpp
@@ -0,0 +1,82 @@
+#ifndef WORDLE_SOLVER_ENTROPY_HPP_
+#define WORDLE_SOLVER_ENTROPY_HPP_
+
+// #include "small_wl.h"
+
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <small_wl.h>
+#include <unordered_map>
+
+/**
+ * @param   ch0 Character n of str0
+ * @param   ch1 Character n of str1
+ * @param   str Word
+ * @return 00 -   grey    - no match \n
+ *         01 -   yellow  - found in word \n
+ *         10 -   green   - ch0 is in the correct position
+ */
+inline std::bitset<2> feedback_char(const char ch0, const char ch1,
+                                    const std::string &str) {
+  std::bitset<2> outcome{};
+
+  const auto it = std::find(str.begin(), str.end(), ch0);
+  const bool found = it != str.end(); // NOLINT move to if-init statement
+
+  if (!found) {
+    return outcome;
+  }
+  if (ch0 == ch1) {
+    outcome.set(1);
+    return outcome;
+  }
+  outcome.set(0);
+  return outcome;
+}
+
+/**
+ * @param [in,out] target Set the bits, as dictated by the section parameter, to
+ *        the source bits
+ * @param source The bit values we wish target to take on
+ * @param section One of 5 partitions of the target bitset
+ */
+inline void set_bits(std::bitset<10> *target, const std::bitset<2> source,
+                     const int section) {
+  const auto index = (target->size() - 1) - (section * 2);
+
+  target->set(index, source[1]);
+  target->set(index - 1, source[0]);
+}
+
+inline std::bitset<10> feedback_word(const std::string &str0,
+                                     const std::string &str1) {
+  std::bitset<10> outcome;
+  for (int i = 0; i < 5; ++i) {
+    set_bits(&outcome, feedback_char(str0[i], str1[i], str1), i);
+  }
+  return outcome;
+}
+
+inline double prob_x(const double x, const double y) { return x / y; }
+
+inline double surprisal(const double prob) { return -std::log2(prob); }
+
+using freq_map = std::unordered_map<std::bitset<10>, double>;
+
+const auto WORDS = static_cast<double>(small_word_list.size());
+
+inline double entropy(const freq_map &feedback) {
+  double entropy{};
+  double sum{};
+  for (const auto &[res, freq] : feedback) {
+    sum += freq;
+  }
+  for (const auto &[res, freq] : feedback) {
+    const auto prob = prob_x(freq, sum);
+    entropy += prob * std::log2(prob);
+  }
+  return -entropy;
+}
+
+#endif // WORDLE_SOLVER_ENTROPY_HPP_
--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,31 @@
+#include "entropy.hpp"
+#include "large_wl.h"
 #include "small_wl.h"
 
 #include <algorithm>
+#include <iomanip>
 #include <iostream>
 #include <map>
 #include <sstream>
-
-#define LOG_Y
-
-static std::map<char, int> lettermap;
-
-std::map<std::string, int> eval_scores(const std::vector<std::string> &vstr);
-
-std::vector<std::pair<int, std::string>>
-list_words_by_score(const std::map<std::string, int> &scores);
+#include <unordered_map>
 
 int main() {
-  // fill letter frequency map -------------------------------------------------
-  for (const auto &word : small_word_list) {
-    for (const auto letter : word) {
-      lettermap[letter]++;
+
+  std::vector<std::pair<double, std::string>> entropies;
+  entropies.reserve(wordlist.size());
+
+  for (const auto &word0 : wordlist) {
+    std::unordered_map<std::bitset<10>, double> feedback;
+    for (const auto &word1 : small_word_list) {
+      ++feedback[feedback_word(word0, word1)];
     }
+    entropies.emplace_back(entropy(feedback), word0);
   }
-#ifdef LOG_Y
-  for (const auto [letter, freq] : lettermap) {
-    std::cout << letter << " " << freq << '\n';
+
+  std::sort(entropies.begin(), entropies.end(), std::less{});
+  for (const auto &[score, word] : entropies) {
+    std::cout << score << " " << word << '\n';
   }
-#endif
-
-  // make commonality/uniqueness score map--------------------------------------
-  std::map<std::string, int> cu_scores = eval_scores(small_word_list);
-
-#ifdef LOG_Y
-  for (const auto &[word, score] : cu_scores) {
-    std::cout << word << " " << score << '\n';
-  }
-#endif
-
-  // sort words based on cu_scores ---------------------------------------------
-  std::vector<std::pair<int, std::string>> r_cu_scores =
-      list_words_by_score(cu_scores);
-
-#ifdef LOG_Y
-  for (const auto &[cu_score, word] : r_cu_scores) {
-    std::cout << cu_score << " " << word << '\n';
-  }
-#endif
-
-  // print first guess ---------------------------------------------------------
-  std::cout << '\n' << r_cu_scores.back().second << '\n';
 
   std::vector<std::string> known_substrings{};
   std::vector<char> include_letters{};
@@ -67,105 +45,21 @@ int main() {
     std::string substring{};
     char ch{};
 
-    std::cout << "\nInput values as prompted, separated by spaces\n\n";
-
-    std::cout << "Letters to exclude: ";
+    std::cout << "Exclude letters: ";
     std::getline(std::cin, input);
     ss.str(input);
     ss.clear();
     while (ss >> ch) {
       exclude_letters.push_back(ch);
     }
-
-#ifdef LOG_Y
+    std::sort(exclude_letters.begin(), exclude_letters.end());
+    exclude_letters.erase(
+        std::unique(exclude_letters.begin(), exclude_letters.end()),
+        exclude_letters.end());
     for (const auto c : exclude_letters) {
       std::cout << c << " ";
     }
     std::cout << '\n';
-#endif
-
-    std::cout << "Letters to include: ";
-    std::getline(std::cin, input);
-
-    ss.str(input);
-    ss.clear();
-    while (ss >> ch) {
-      include_letters.push_back(ch);
-    }
-
-#ifdef LOG_Y
-    for (const auto c : include_letters) {
-      std::cout << c << " ";
-    }
-    std::cout << '\n';
-#endif
-
-    std::cout << "Known substrings: ";
-
-    std::getline(std::cin, input);
-
-    ss.str(input);
-    ss.clear();
-    while (ss >> substring) {
-      known_substrings.push_back(substring);
-    }
-
-#ifdef LOG_Y
-    for (const auto &strings : known_substrings) {
-      std::cout << strings << " ";
-    }
-    std::cout << '\n';
-#endif
-
-    std::cout << "Letters in known positions: ";
-
-    std::getline(std::cin, input);
-
-    if (!input.empty()) {
-      for (auto j = 0; j < 5; ++j) {
-        if (input[j] != '0') {
-          pos_positions.push_back(j);
-        }
-        letters_go_here[j] = input[j];
-      }
-    }
-
-    std::sort(pos_positions.begin(), pos_positions.end());
-    pos_positions.erase(std::unique(pos_positions.begin(), pos_positions.end()),
-                        pos_positions.end());
-
-#ifdef LOG_Y
-    for (const auto ch : letters_go_here) {
-      std::cout << ch;
-    }
-    std::cout << '\n';
-#endif
-
-    std::cout << "Letters must not be here: ";
-
-    std::getline(std::cin, input);
-
-    if (!input.empty()) {
-      for (auto j = 0; j < 5; ++j) {
-        if (input[j] != '0') {
-          neg_positions.push_back(j);
-        }
-        letters_dont_go_here[j] = input[j];
-      }
-    }
-
-    std::sort(neg_positions.begin(), neg_positions.end());
-    neg_positions.erase(std::unique(neg_positions.begin(), neg_positions.end()),
-                        neg_positions.end());
-
-#ifdef LOG_Y
-    for (const auto ch : letters_dont_go_here) {
-      std::cout << ch;
-    }
-    std::cout << '\n';
-#endif
-
-    // exclude letters
     if (i == 0) {
       for (const auto &word : small_word_list) {
         bool found{false};
@@ -196,7 +90,22 @@ int main() {
       }
     }
 
-    // include letters
+    std::cout << "Include letters: ";
+    std::getline(std::cin, input);
+    ss.str(input);
+    ss.clear();
+    while (ss >> ch) {
+      include_letters.push_back(ch);
+    }
+    std::sort(include_letters.begin(), include_letters.end());
+    include_letters.erase(
+        std::unique(include_letters.begin(), include_letters.end()),
+        include_letters.end());
+    for (const auto c : include_letters) {
+      std::cout << c << " ";
+    }
+    std::cout << '\n';
+
     for (auto word = possible_words.begin(); word != possible_words.end();) {
       bool found{true};
       for (const auto letter : include_letters) {
@@ -212,23 +121,23 @@ int main() {
       }
     }
 
-    // include substrings
-    for (auto word = possible_words.begin(); word != possible_words.end();) {
-      bool found{true};
-      for (const auto &substring : known_substrings) {
-        if (word->find(substring) == std::string::npos) {
-          found = false;
-          break;
+    std::cout << "Letters in known positions: ";
+    std::getline(std::cin, input);
+    if (!input.empty()) {
+      for (auto j = 0; j < 5; ++j) {
+        if (input[j] != '0') {
+          pos_positions.push_back(j);
         }
-      }
-      if (!found) {
-        word = possible_words.erase(word);
-      } else {
-        ++word;
+        letters_go_here[j] = input[j];
       }
     }
-
-    // letters in known positions
+    for (const auto chr : letters_go_here) {
+      std::cout << chr;
+    }
+    std::cout << '\n';
+    std::sort(pos_positions.begin(), pos_positions.end());
+    pos_positions.erase(std::unique(pos_positions.begin(), pos_positions.end()),
+                        pos_positions.end());
     for (auto word = possible_words.begin(); word != possible_words.end();) {
       bool found{true};
       for (const auto index : pos_positions) {
@@ -244,7 +153,23 @@ int main() {
       }
     }
 
-    // letters don't go here
+    std::cout << "Letters must not be here: ";
+    std::getline(std::cin, input);
+    if (!input.empty()) {
+      for (auto j = 0; j < 5; ++j) {
+        if (input[j] != '0') {
+          neg_positions.push_back(j);
+        }
+        letters_dont_go_here[j] = input[j];
+      }
+    }
+    std::sort(neg_positions.begin(), neg_positions.end());
+    neg_positions.erase(std::unique(neg_positions.begin(), neg_positions.end()),
+                        neg_positions.end());
+    for (const auto chr : letters_dont_go_here) {
+      std::cout << chr;
+    }
+    std::cout << '\n';
     for (auto word = possible_words.begin(); word != possible_words.end();) {
       bool found{false};
       for (const auto index : neg_positions) {
@@ -260,42 +185,38 @@ int main() {
       }
     }
 
-    auto scores = eval_scores(possible_words);
-    auto list_scores = list_words_by_score(scores);
-    for (const auto &[score, word] : list_scores) {
-      std::cout << score << " " << word << '\n';
-    }
+    if (possible_words.size() == 1) {
+      std::cout << possible_words.front() << '\n';
+    } else {
 
-    std::cout << double(possible_words.size()) / double(small_word_list.size())
-              << '\n';
+      entropies.clear();
+      entropies.reserve(possible_words.size());
+
+      auto select_list = [&]() -> const std::vector<std::string> * {
+        if (i == 0 && possible_words.size() > 5) {
+          return &wordlist;
+        }
+        if (possible_words.size() <= 5) {
+          return &possible_words;
+        }
+        return &small_word_list;
+      };
+
+      for (const auto &word0 : *select_list()) {
+        std::unordered_map<std::bitset<10>, double> feedback;
+        for (const auto &word1 : possible_words) {
+          ++feedback[feedback_word(word0, word1)];
+        }
+        entropies.emplace_back(entropy(feedback), word0);
+      }
+
+      std::sort(entropies.begin(), entropies.end(), std::less{});
+      for (const auto &[score, word] : entropies) {
+        std::cout << std::setprecision(3) << score << " " << word << '\n';
+      }
+
+      std::cout << possible_words.size() << '\n';
+    }
   }
   return 0;
-}
-
-std::map<std::string, int> eval_scores(const std::vector<std::string> &vstr) {
-  std::map<std::string, int> scores;
-  for (const auto &word : vstr) {
-    int score{};
-    std::string seenletters;
-    for (const auto letter : word) {
-      if (seenletters.find(letter) == std::string::npos) {
-        score += lettermap[letter];
-        seenletters += letter;
-      }
-    }
-    score *= seenletters.size(); // NOLINT
-    scores[word] = score;
-  }
-  return scores;
-}
-
-std::vector<std::pair<int, std::string>>
-list_words_by_score(const std::map<std::string, int> &scores) {
-  std::vector<std::pair<int, std::string>> score_list;
-  score_list.reserve(scores.size());
-  for (const auto &[word, cu_score] : scores) {
-    score_list.emplace_back(cu_score, word);
-  }
-  std::sort(score_list.begin(), score_list.end());
-  return score_list;
 }
