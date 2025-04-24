a607486cb90d77b12d448e28e674fd0e641f3b87
schauss <schauss@dmanz.org>
2025-04-24 14:33:09 -0700

    Revert "an experiment ... I don't think it's worth re-writing everything with arrays ..."
    This reverts commit 973a1309b25b3000e88ef6d7a9f49839754c696e.

--- solver/constexpr_test/Words.cpp
+++
@@ -1,6 +0,0 @@
-#include "Words.hpp"
-
-extern const std::array<Words::Word_t, Words::n_words> Words::all =
-    Words::load(ALL_WORDS_TXT);
-extern const std::array<Words::Word_t, Words::n_solutions> Words::solutions =
-    Words::load(ALL_SOLUTIONS_TXT);
